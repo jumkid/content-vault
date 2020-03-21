145d98e96f861b8958c87dcbba628e6e8185bbe9
chooli <chooli.yip@gmail.com>
2020-03-21 17:36:36 -0400

    Project initiation

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# file-vault
+# File vault
+a file service provides media file storage on local file system as well as cloud-based file system. It also provide media accessing API and streaming services
+
+# Get Start
+Please clone or download this project to your local. Say C:/git/media-server
+Type the command below
+
+<code>cd c:/git</code>
+
+<code>git clone https://github.com/jumkid/file-vault.git</code>
+
+# Prerequisites
+Please install jdk 11 on your local machine. And type to following command to verify it
+
+<code>java -version</code>
+
+# How to Install
---
+++ src/main/java/com/jumkid/vault/FileVaultApplication.java
@@ -0,0 +1,52 @@
+package com.jumkid.vault;
+
+/*
+ * This software is written by Jumkid and subject
+ * to a contract between Jumkid and its customer.
+ *
+ * This software stays property of Jumkid unless differing
+ * arrangements between Jumkid and its customer apply.
+ *
+ *
+ * (c)2019 Jumkid All rights reserved.
+ *
+ */
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+
+/**
+ * Main application endpoint with spring boot
+ *
+ * Created at Sep2018$
+ *
+ * @author chooliyip
+ **/
+@Slf4j
+@SpringBootApplication
+public class FileVaultApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileVaultApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("File Vault Application started with command-line arguments: {} . \n To kill this application, press Ctrl + C.",
+                Arrays.toString(args));
+    }
+
+    @Bean(name = "restTemplate")
+    public RestTemplate restTemplateBean(){
+        return new RestTemplate();
+    }
+
+}
